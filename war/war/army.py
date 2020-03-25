6eb178a2105e944bb444a36811d5fd25de96a77f
Pranav Kohli <pranav.kohli.90@gmail.com>
2020-03-25 20:20:22 +0530

    added army class

---
+++ war/war/army.py
@@ -0,0 +1,8 @@
+class Army:
+
+    def __init__(self, name, horses, elephants, armouredTanks, slingGuns):
+        self.name = name
+        self.horses = horses
+        self.elephants = elephants
+        self.armouredTanks = armouredTanks
+        self.slingGuns = slingGuns
